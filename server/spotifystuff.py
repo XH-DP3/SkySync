1a6a8591b9936a6aff22d1b343086494117f2301
Xinghao Huang <xhuang69@students.cs.ubc.ca>
2025-11-16 09:52:38 -0800

    update files

--- server/spotifystuff.py
+++ server/spotifystuff.py
@@ -19,7 +19,7 @@ auth_manager = SpotifyOAuth(client_id=client_id,
                             cache_path=".cache")
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-playlist_id = '0JF1rP1whCmJt4SyeQaJve'  # example Spotify playlist URI or ID
+playlist_id = '0JiVp7Z0pYKI8diUV6HJyQ'  # example Spotify playlist URI or ID
 
 
 
